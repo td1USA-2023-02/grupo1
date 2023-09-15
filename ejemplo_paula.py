83cf89a6e4332ecad42881bfa7fc505893d18875
PaulaGSUni <maria.guerrero05@usa.edu.co>
2023-09-14 19:11:47 -0500

    Borre la grafica

---
+++ ejemplo_paula.py
@@ -0,0 +1,11 @@
+import ejemplo_sergio 
+print ("Hola Mundo")
+print ("Este es un mensaje")
+
+print ("Hola, hice un merge y cambie a mi rama")
+print (ejemplo_sergio.lista_resultados)
+
+
+
+
+    
