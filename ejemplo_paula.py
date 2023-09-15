bf516e2bedab1accdb6a29b8d9e13eab953a3456
unknown <sergio.palencia01@usa.edu.co>
2023-09-14 19:03:01 -0500

    grafico y cambio nombre

---
+++ ejemplo_paula.py
@@ -0,0 +1,15 @@
+import ejemplo_sergio 
+print ("Hola Mundo")
+print ("Este es un mensaje")
+
+print ("Hola, hice un merge y cambie a mi rama")
+print (ejemplo_sergio.lista_resultados)
+
+import matplotlib.pyplot as plt
+    datos=[ejemplo_sergio.lista_resultados]
+    plt.plot(datos)
+    plt.xlabel('Eje X')
+    plt.ylabel('Eje Y')
+    plt.title('Gráfico de Ejemplo')
+    plt.show()
+    
